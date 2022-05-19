cda2ff90e062b3fef16d2f42f992b8336a0789d8
Lars <larsbrogaard86@gmail.com>
2022-05-19 15:01:00 +0200

    Lars

--- settings.js
+++ settings.js
@@ -1,3 +1,10 @@
 export const apiRoot = "https://hawaiibio.azurewebsites.net/api/"
 
-    //http://localhost:8090/api/
+
+//"https://hawaiibio.azurewebsites.net/api/"
+
+    
+
+   
+
+   //"http://localhost:8090/api/"
